0749fd6bf2392beb3c0135d4c719f9a96b64ef98
Gaurav-Rai121 <raigaurav9364@gmail.com>
2025-05-05 21:28:15 +0530

    add new files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$number="@123456";
+
+$hasNumber=ctype_digit($number);
+
+if($hasNumber){
+    echo'hello';
+}
+
+else{
+    echo 'bo ';
+}ijer3fhoi3hro3h4ro34rnfoih34oihoih34oihrfoioineorfnoernf
+
+?>
